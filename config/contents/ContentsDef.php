2667c7178cf630b42be364fa988d06978cc16d29
kouji.kawada <kawadakoujisun@gmail.com>
2021-04-26 11:19:53 +0000

    add japanese error messages

--- config/contents/ContentsDef.php
+++ config/contents/ContentsDef.php
@@ -9,10 +9,12 @@ return [
         'RECENT_APPEARANCE' => '最近登場した順',
     ],
     
+    'IMAGE_FILE_SIZE_MAX' => '2048',  // 写真のサイズの最大値(KB)  // 'IMAGE_FILE_SIZE_MAX'と'requestValidateValueArray'に2048という記述あり。
+
     'requestValidateValueArray' => [
         'date'                => 'required | date | unique:dates',
         'name'                => 'required | max:127',
         'description'         => 'required | max:511',
-        'selected_image_file' => 'mimes:jpeg,jpg,gif,png,bmp | max:2048',  // requiredが必要なときは足して
+        'selected_image_file' => 'mimes:jpeg,jpg,gif,png,bmp | max:2048',  // requiredが必要なときは足して  // 'IMAGE_FILE_SIZE_MAX'と'requestValidateValueArray'に2048という記述あり。
     ],
 ];
---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+    
+    'required' => ' :attribute は必須です。',
+    'date'     => ' :attribute が正しい日付ではありません。',          // 未確認
+    'unique'   => '既に存在している :attribute です。',
+    'max' => [
+        'file'   => ' :attribute は :max KB以下にしてください。',      // 未確認
+        'string' => ' :attribute は :max 文字以下にしてください。',
+    ],
+    
+    'uploaded' => ' :attribute をアップロードできませんでした。サイズは' . \Config::get('contents.ContentsDef.IMAGE_FILE_SIZE_MAX') . 'KB以下でなければなりませんが、それより大きい場合はそれが原因かもしれません。',
+    'mimes'    => ' :attribute のフォーマットは :values にしてください。',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [
+        'date'                => '日にち',
+        'name'                => '料理名',
+        'description'         => '説明',
+        'selected_image_file' => '写真',
+        
+        'email'    => 'メールアドレス',
+        'password' => 'パスワード'
+    ],
+
+];
