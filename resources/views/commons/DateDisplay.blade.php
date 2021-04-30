05333c3d914e36fd14d71c580b0dd117f87d899f
kouji.kawada <kawadakoujisun@gmail.com>
2021-04-30 11:03:38 +0000

    modify date display

--- resources/views/commons/DateDisplay.blade.php
+++ resources/views/commons/DateDisplay.blade.php
@@ -7,5 +7,8 @@
         $dayOfTheWeekNameArray = [ '日', '月', '火', '水', '木', '金', '土' ];
         $dateDst = date('Y', $dateTimestamp) . '年' . date('n', $dateTimestamp) . '月' . date('j', $dateTimestamp) . '日' . '（' . $dayOfTheWeekNameArray[date('w', $dateTimestamp)] . '曜日）';
     }
-    print $beforeDate . $dateDst . $afterDate;  // htmlタグあり改行なしなのでhtmlnl2br(htmlspecialchars())では括らない。ユーザが文字列を入力するわけではないし。
+    // print $beforeDate . $dateDst . $afterDate;  // htmlタグあり改行なしなのでnl2br(htmlspecialchars())では括らない。ユーザが文字列を入力するわけではないし。
+    // でも{!! !!}と{{ }}で表示することにしたのでコメントアウト。表示方法を変えるに至った理由は特にないけど。
 ?>
+
+{!! $beforeDate !!} {{ $dateDst }} {!! $afterDate !!}
